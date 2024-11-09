19534ab6825ea630be7af844590d9c19a1c67e76
Prozi Jacek Pietal <prozi85@gmail.com>
2024-11-09 23:13:37 +0100

    fix: for time 0

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = require("lodash");
 const oldSetTimeout = setTimeout;
 const oldSetInterval = setInterval;
-globalThis.setTimeout = ((callback, ms = 0) => oldSetTimeout((0, lodash_1.throttle)(callback, ms), ms));
-globalThis.setInterval = ((callback, ms = 0) => oldSetInterval((0, lodash_1.throttle)(callback, ms), ms));
+globalThis.setTimeout = (callback, ms = 0) =>
+  oldSetTimeout(ms ? (0, lodash_1.throttle)(callback, ms) : callback, ms);
+globalThis.setInterval = (callback, ms = 0) =>
+  oldSetInterval(ms ? (0, lodash_1.throttle)(callback, ms) : callback, ms);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { throttle } from 'lodash';
+import { throttle } from "lodash";
 
 const oldSetTimeout = setTimeout;
 const oldSetInterval = setInterval;
 
 globalThis.setTimeout = ((callback: (args: void) => void, ms = 0) =>
-  oldSetTimeout(throttle(callback, ms), ms)) as any;
+  oldSetTimeout(ms ? throttle(callback, ms) : callback, ms)) as any;
 globalThis.setInterval = ((callback: (args: void) => void, ms = 0) =>
-  oldSetInterval(throttle(callback, ms), ms)) as any;
+  oldSetInterval(ms ? throttle(callback, ms) : callback, ms)) as any;
